302688a7f6b5bbe3896efceed82ddd1d632cf727
Max62c <max62c@gmail.com>
2022-12-08 12:42:22 +0100

    Delete Inscription directory

--- Inscription/config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-     try 
-    {
-        $bdd = new PDO("mysql:host=LOCALHOST;dbname=test;charset=utf8", "root", "maxime");
-    }
-    catch(PDOException $e)
-    {
-        die('Erreur : '.$e->getMessage());
-    }
--- Inscription/deconnexion.php
+++
@@ -1,5 +0,0 @@
-<?php 
-    session_start(); // demarrage de la session
-    session_destroy(); // on détruit la/les session(s)
-    header('Location:index.php'); // On redirige
-    die();
